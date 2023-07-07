fd74fb2896c4e73a4f02f2af85a103459c03db9d
akrem <akrem@NIKO>
2023-07-08 00:16:13 +0400

    Add project files.

---
+++ ProductSIte/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProductSIte.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ProductSIte/Models/Products.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductSIte.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int price { get; set; }
+        public int quantity { get; set; }
+    }
+}

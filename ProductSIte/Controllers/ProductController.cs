bd02b1246aae293996d4db9ee1121456d9472a2a
akrem <akrem@NIKO>
2023-07-08 23:36:32 +0400

    update 2.2

--- ProductSIte/Controllers/ProductController.cs
+++ ProductSIte/Controllers/ProductController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProductSIte.Models;
 
 namespace ProductSIte.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private List<Products> products = new()
         {
-            return View();
+            new Products {Id = 1 , Name= "Alma" , Description = "Quba Almasi" , price =1.20f, quantity = 10},
+            new Products {Id = 2 ,Name = "Bizon" , Description = "Enerji ickisi" , price = 1.50f , quantity = 200},
+            new Products {Id = 3 ,Name = "Redbull" , Description = "Qanadlandirir" , price = 2.50f , quantity=200},
+        };
+
+        //[HttpGet]
+        public IActionResult AllProducts()
+        {
+            return View(products);
         }
     }
 }
--- ProductSIte/Models/Products.cs
+++ ProductSIte/Models/Products.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public int price { get; set; }
+        public float price { get; set; }
         public int quantity { get; set; }
     }
 }

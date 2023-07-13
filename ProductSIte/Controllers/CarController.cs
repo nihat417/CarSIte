625fbeb9d9d67a592dbaf06f056fd6326bc5987a
akrem <akrem@NIKO>
2023-07-13 13:26:00 +0400

    //

--- ProductSIte/Controllers/CarController.cs
+++ ProductSIte/Controllers/CarController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarSite.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using ProductSIte.Models;
 
 namespace ProductSIte.Controllers
@@ -7,20 +8,19 @@ namespace ProductSIte.Controllers
     {
         private static List<Car> cars = new()
         {
-            new Car { Id = 6,Make = "Hyundai",Model = "Elantra",Price = 28000,Year = 2020,Color = "Silver",EngineSize = 1600,FuelEfficiency = 34.2,photos = "/Images/elantra.jpg"},
-            new Car { Id = 2,Make = "Ford",Model = "Mustang",Price = 45000,Year = 2023,Color = "Black",EngineSize = 2500,FuelEfficiency = 25.8, photos = "/Images/mustang.jpg"},
-            new Car { Id = 3,Make = "Chevrolet",Model = "Camaro",Price = 42000,Year = 2022,Color = "Yellow",EngineSize = 3000,FuelEfficiency = 24.6, photos = "/Images/camaro.jpg"},
-            new Car { Id = 6,Make = "BMW",Model = "3 Series",Price = 40000,Year = 2021,Color = "Blue",EngineSize = 2000,FuelEfficiency = 28.5,photos = "/Images/bmw3ser.jpg"},
-            new Car { Id = 5,Make = "Toyota",Model = "Supra",Price = 45000,Year = 2012,Color = "Dark",EngineSize = 10000,FuelEfficiency = 90.5 ,photos = "/Images/supra.jpg"},
-            new Car { Id = 0,Make = "Toyota",Model = "Camry",Price = 25000,Year = 2022,Color = "Silver",EngineSize = 2000,FuelEfficiency = 30.5 ,photos = ""},
-            new Car { Id = 4,Make = "BMW",Model = "X5",Price = 65000,Year = 2023,Color = "White",EngineSize = 3500,FuelEfficiency = 22.3, photos = "/Images/bmwx5jpg.jpg"},
-            new Car { Id = 1,Make = "Honda",Model = "Accord",Price = 28000,Year = 2021,Color = "Red",EngineSize = 1800,FuelEfficiency = 32.1,photos = "/Images/HondaAccord.jpg"},
-            new Car { Id = 7,Make = "Ford",Model = "Mustang GT",Price = 50000,Year = 2022,Color = "Red",EngineSize = 5000,FuelEfficiency = 20.1,photos = ""},
-            new Car { Id = 8,Make = "Mercedes-Benz",Model = "C-Class",Price = 45000,Year = 2020,Color = "White",EngineSize = 2500,FuelEfficiency = 26.8,photos = "/Images/mers.jpg"},
+            new Car { Id = 0,Make = "Hyundai",Model = "Elantra",Price = 28000,Year = 2020,Color = "Silver",EngineSize = 1600,FuelEfficiency = 34.2,photos = "/Images/elantra.jpg"},
+            new Car { Id = 1,Make = "Ford",Model = "Mustang",Price = 45000,Year = 2023,Color = "Black",EngineSize = 2500,FuelEfficiency = 25.8, photos = "/Images/mustang.jpg"},
+            new Car { Id = 2,Make = "Chevrolet",Model = "Camaro",Price = 42000,Year = 2022,Color = "Yellow",EngineSize = 3000,FuelEfficiency = 24.6, photos = "/Images/camaro.jpg"},
+            new Car { Id = 3,Make = "BMW",Model = "3 Series",Price = 40000,Year = 2021,Color = "Blue",EngineSize = 2000,FuelEfficiency = 28.5,photos = "/Images/bmw3ser.jpg"},
+            new Car { Id = 4,Make = "Toyota",Model = "Supra",Price = 45000,Year = 2012,Color = "Dark",EngineSize = 10000,FuelEfficiency = 90.5 ,photos = "/Images/supra.jpg"},
+            new Car { Id = 5,Make = "Toyota",Model = "Camry",Price = 25000,Year = 2022,Color = "Silver",EngineSize = 2000,FuelEfficiency = 30.5 ,photos = ""},
+            new Car { Id = 6,Make = "BMW",Model = "X5",Price = 65000,Year = 2023,Color = "White",EngineSize = 3500,FuelEfficiency = 22.3, photos = "/Images/bmwx5jpg.jpg"},
+            new Car { Id = 7,Make = "Honda",Model = "Accord",Price = 28000,Year = 2021,Color = "Red",EngineSize = 1800,FuelEfficiency = 32.1,photos = "/Images/HondaAccord.jpg"},
+            new Car { Id = 8,Make = "Ford",Model = "Mustang GT",Price = 50000,Year = 2022,Color = "Red",EngineSize = 5000,FuelEfficiency = 20.1,photos = ""},
+            new Car { Id = 9,Make = "Mercedes-Benz",Model = "C-Class",Price = 45000,Year = 2020,Color = "White",EngineSize = 2500,FuelEfficiency = 26.8,photos = "/Images/mers.jpg"},
+        };
 
-
-
-    };
+        private static int _Id=10;
 
         [HttpGet]
         public IActionResult AddCar()
@@ -28,11 +28,30 @@ namespace ProductSIte.Controllers
             return View();
         }
 
+        //[Route("Addcar")]
         [HttpPost]
-        public IActionResult AddCar(Car car)
+        public IActionResult AddCar(AddCarVM model)
         {
-            cars.Add(car);
-            return RedirectToAction("AllCars");
+            if(ModelState.IsValid)
+            {
+                Car car = new()
+                {
+                    Id = _Id++,
+                    Make= model.Make,
+                    Model = model.Model,
+                    Price = model.Price,
+                    Year = model.Year,
+                    Color = model.Color,
+                    EngineSize = model.EngineSize,
+                    FuelEfficiency = model.FuelEfficiency,
+                    //photos = model.photos,
+                };
+                cars.Add(car);
+                return RedirectToAction("AllCars");
+            }
+            //car.Id = Id++;
+            //cars.Add(car);
+            return View("Addcar");
         }
 
         public IActionResult DeleteCar(int id)
---
+++ ProductSIte/Models/ViewModels/AddCarVM.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarSite.Models.ViewModels
+{
+    public class AddCarVM
+    {
+        [Required(ErrorMessage = "Make is required")]
+        public string? Make { get; set; }
+
+        [Required(ErrorMessage = "Model is required")]
+        public string? Model { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Price must be a positive number")]
+        public int Price { get; set; }
+
+        [Range(1900, int.MaxValue, ErrorMessage = "Year must be greater than 1900")]
+        public int Year { get; set; }
+
+        [Required]
+        public string? Color { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "EngineSize must be a positive number")]
+        public int EngineSize { get; set; }
+
+        [Range(0.1, double.MaxValue, ErrorMessage = "FuelEfficiency must be a positive number")]
+        public double FuelEfficiency { get; set; }
+    }
+}
